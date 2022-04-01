320a7b4802f81b6813d80556eba272560e2b0335
SirHarveyBix <gui.lefort.17@gmail.com>
2022-04-01 12:29:09 +0200

    create config for webPack

--- Project/src/components/project-input.ts
+++ Project/src/components/project-input.ts
@@ -1,7 +1,7 @@
-import { autobind as Autobind } from '../decorators/autobind.js';
-import { projectState } from '../state/project-state.js';
-import * as Validation from '../utils/validation.js';
-import Component from './base-component.js';
+import { autobind as Autobind } from '../decorators/autobind';
+import { projectState } from '../state/project-state';
+import * as Validation from '../utils/validation';
+import Component from './base-component';
 
 export class Projectinput extends Component<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
