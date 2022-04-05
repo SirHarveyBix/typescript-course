69cc41b8835a4e3cd7964bc67a892787c76c16ce
SirHarvey <gui.lefort.17@gmail.com>
2022-04-05 13:34:40 +0200

    add full CRUD tested > Postman : 👌

--- node-express/src/controllers/todos.ts
+++ node-express/src/controllers/todos.ts
@@ -10,6 +10,31 @@ export const createTodo: RequestHandler = (request, response, next) => {
 
   TODOS.push(newTodo);
 
-  console.log(request);
   response.status(201).json({ message: 'Created !', createdTodo: newTodo });
+};
+
+export const getTodos: RequestHandler = (request, response, next) => {
+  response.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (request, response, next) => {
+  const todoId = request.params.id;
+
+  const updatedText = (<{ text: string }>request.body).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) throw new Error("Couldn't find your todo :( ");
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  response.json({ message: 'Updated !', updateTodo: TODOS[todoIndex] });
+};
+export const deleteTodo: RequestHandler<{ id: string }> = (request, response, next) => {
+  const todoId = request.params.id;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) throw new Error("Couldn't find your todo :( ");
+
+  TODOS.splice(todoIndex, 1);
+  response.json({ message: 'Todo Deleted' });
 };
